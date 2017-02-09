01c4d77911f67c80c0315c669f5026fe7ebdcbe8
Sigrid Passano Hellan <sighellan@gmail.com>
2017-02-09 22:10:24 +0000

    Simple example of reading in lines from a text file.

---
+++ readingFile.fsx
@@ -0,0 +1,9 @@
+﻿open System.IO
+open System
+
+let myPath = @"C:\cygwin64\fsharp\project\textFile.txt"
+
+let readFile pathString = IO.File.ReadLines pathString
+
+let fileLines = readFile myPath
+fileLines
