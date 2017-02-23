2b82549fa32fe1534ebc0f0b9437288a3b304ecd
Sigrid Passano Hellan <sighellan@gmail.com>
2017-02-23 14:30:01 +0000

    Draft of how data can be structured internally.

--- readingFile.fsx
+++ readingFile.fsx
@@ -1,6 +1,11 @@
 ﻿    open System.IO
     open System
-    type myData = | String of Option<string> | Int of Option<int> | Float of Option<float>
+    type myData = | String of Option<string> | Int of Option<int> | Float of Option<float>                // To be extended when the wanted data types have been decided
+    type IList = INode of ParName: string * Value: myData * Prev : ref<IList> * Tl : ref<IList> | INilLow // A node has the parameter name (column name), the parameter value
+                                                                                                          // and links to the previous and next nodes
+    type topList = Node of lowList : ref<IList> * Tl : ref<topList> | INilTop                             // The nodes of the top-level list have refs to low-level lists.
+    type data = ref<topList>                                                                              // Holds all the data in the database
+     
 
     let myPath = @"C:\Users\Sigrid\Documents\Visual Studio 2015\HLP\dataFile.txt"
 
@@ -8,6 +13,26 @@
 
     let fileLines = readFile myPath
 
+    let head1  = ref INilLow
+    let tail1  = ref INilLow
+    let test2 = ref INilLow
+    let test1 = ref (INode ("username", (Some >> String) "Orlando", head1, test2))
+    test2 := INode ("age", (int >> Some >> Int) "45", test1, tail1)
+    test1
+
+    let head2 = ref INilLow
+    let tail2 = ref INilLow
+    let test4 = ref INilLow
+    let test3 = ref (INode ("username", (Some >> String) "Rebecca", head2, test4))
+
+    test4 := INode ("age", (int >> Some >> Int) "17", test3, tail2)
+    test3
+    
+    let tmpT  = ref INilTop
+    let tailT = ref INilTop
+    let myTopList = (Node (test1, tmpT))
+    tmpT := Node (test3, tailT)
+    myTopList
 
     let rec strProcess = function
         | [] -> []
