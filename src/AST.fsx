6dcb42b71ff4bfc9611e255d078ff18b82709e87
Matt Abdul-Rahim <Matthewar@users.noreply.github.com>
2017-02-26 21:35:08 +0000

    Completed wrapped column list matching.

--- src/AST.fsx
+++ src/AST.fsx
@@ -17,23 +17,55 @@ module AST =
       | As
       | Condition
       | Variable
+      | Column
+      | Alias
 
    type node =
       | Branch of Name:node * Children:node list
+      | Item of Name:node * Child:node
       | Literal of Token.content
       | Key of keyword
       | Null
 
-   type ReturnCode =
-      | Result of node
+   type 'a ReturnCode =
+      | Result of 'a
       | Error of string
 
-   let ColumnWrappedList (tokenList:Tokeniser.tokens) = //??
+   let processConsOnReturn (item:node) (rest:ReturnCode<node list>) =
+      match rest with
+      | ReturnCode<node list>.Result(res) -> ReturnCode<node list>.Result(item :: res)
+      | ReturnCode<node list>.Error(s) -> ReturnCode<node list>.Error(s)
+
+   let ColumnWrappedList (tokenList:Tokeniser.tokens) =
+      let (|FunctionMatch|_|) (lst:Tokeniser.tokens) =
+         let validFunctions = [| "AVG" ; "MAX" ; "MIN" ; "SUM" ; "ROUND" |]
+         match lst with
+         | Token.content.Name(funName) :: Token.content.Operator("(") :: Token.content.Name(colName) :: Token.content.Operator(")") :: rest when Array.contains funName validFunctions ->
+            [node.Item(keyword.Function,Token.content.Name(funName)) ; node.Item(keyword.Name,Token.content.Name(colName))]
+            |> fun x -> Some(x,rest)
+         | _ -> None
+      let (|AliasMatch|_|) (lst:Tokeniser.tokens) =
+         match lst with
+         | Token.content.Name(colName) :: Token.content.Name("AS") :: Token.content.Literal(colAlias) :: rest ->
+            [node.Item(keyword.Name,Token.content.Name(colName)) ; node.Item(keyword.Alias,Token.content.Literal(colAlias))]
+            |> fun x -> Some(x,rest)
+         | _ -> None
       let rec parse (lst:Tokeniser.tokens) (nextColumn:bool) =
          match lst with
-         | Token.content.Name(columnName) :: rest when nextColumn -> node.Literal(Token.content.Name(columnName)) :: parse rest false
+         | FunctionMatch (funLst,rest) when nextColumn ->
+            processConsOnReturn node.Branch(keyword.Function,funLst) (parse rest false)
+         | AliasMatch (aliasLst,rest) when nextColumn ->
+            processConsOnReturn node.Branch(keyword.Alias,aliasLst) (parse rest false)
+         | Token.content.Name(name) when nextColumn ->
+            processConsOnReturn node.Item(keyword.Name,node.Literal(Token.content.Name(name))) (parse rest false)
          | Token.content.Operator(",") :: rest when not nextColumn -> parse rest true
+         | item :: rest when nextColumn -> ReturnCode<node list>.Error(printfn "Expected wrapped column name, got %A" item)
+         | _ -> ReturnCode<node list>.Result([])
       parse tokenList true
+      |> fun x ->
+         match x with
+         | ReturnCode<node list>.Result(columnLst,rest) -> ReturnCode<node*node list>.Result(node.Branch(keyword.Column,columnLst),rest)
+         | ReturnCode<node list>.Error(str) -> ReturnCode<node*node list>.Error(str)
 
    let (|BranchMatch|_|) (tokenList:Tokeniser.tokens) =
       let output (key:keyword) (result:node*Tokeniser.tokens) =
