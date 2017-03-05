5c9bbbbd69454b9d06dfbddcc50a6078022aa4c1
Sigrid Passano Hellan <sighellan@gmail.com>
2017-03-05 10:57:16 +0000

    Now also reads in bytes and booleans, and correctly reads in files with tabs, and data where last element is not present.

--- readingFile.fsx
+++ readingFile.fsx
@@ -1,6 +1,7 @@
 ﻿    open System.IO
     open System
     type myData = | String of Option<string> | Int of Option<int> | Float of Option<float>                // To be extended when the wanted data types have been decided
+                  | Byte of Option<byte>     | Bool of Option<bool>
     type IList = INode of ParName: string * Value: myData * Prev : ref<IList> * Tl : ref<IList> | INilLow // A node has the parameter name (column name), the parameter value
                                                                                                           // and links to the previous and next nodes
     type topList = Node of lowList : ref<IList> * Tl : ref<topList> | INilTop                             // The nodes of the top-level list have refs to low-level lists.
@@ -34,30 +35,43 @@
     tmpT := Node (test3, tailT)
     myTopList
 
+    let helperfunc list f F caller emes=
+        match list with
+        | "Some" :: c :: tail -> (c |> f |> Some |> F) :: caller tail
+        | "None" :: tail -> F None :: caller tail
+        | e :: tail -> (Some >> String) ("error: " + emes + ": " + e) :: caller tail
+        | [] -> [(Some >> String) ("error: " + emes + " empty list")]
+
     let rec strProcess = function
         | [] -> []
-        | a :: b :: c :: tail when a = "String" -> 
-            if b = "Some" then (Some >> String) c :: (strProcess tail)
-            else String None :: strProcess (c :: tail)
-        | a :: b :: c :: tail when a = "Int"    -> 
-            if b = "Some" then (int >> Some >> Int) c :: (strProcess tail) 
-            else Int None :: strProcess (c :: tail)
-        | a :: b :: c :: tail when a = "Float"  -> 
-            if b = "Some" then (float >> Some >> Float) c :: (strProcess tail) 
-            else Float None :: strProcess (c :: tail)
-        | e :: tail                             -> (Some >> String) "error" :: (strProcess tail)
-
+        | a :: tail when a = "String" -> helperfunc tail (fun x -> x) String strProcess a
+        | a :: tail when a = "Int"    -> helperfunc tail int Int strProcess a
+        | a :: tail when a = "Float"  -> helperfunc tail float Float strProcess a
+        | a :: tail when a = "Byte"   -> helperfunc tail byte Byte strProcess a
+        | a :: tail when a = "Bool"   -> helperfunc tail System.Convert.ToBoolean Bool strProcess a
+        | e :: tail                   -> (Some >> String) ("error: " + e) :: (strProcess tail)
+         
+    // Tests
     strProcess ["String"; "Some"; "Orlando"; "Int"; "Some"; "45"]
+    strProcess ["Byte"; "Some"; "0xF3"; "Byte"; "None"]
+    strProcess ["Bool"; "Some"; "True"; "Bool"; "None"]
+    strProcess ["Float"; "Some"; "3.1415926"; "Float"; "None"]
+    strProcess ["String"; "some"; "hei"]
+    strProcess ["Strange"; "Some"; "Dragon"]
+
+
 
     let readInLine (inpString : string) =
-        inpString.Split [|' '|] 
+        inpString.Split [|' ';'\t'|] 
         |> Array.toList |> List.filter (fun x -> x <> "")
         |> strProcess
+
 
     let readIn mySeq =
         mySeq |> Seq.toList |> List.map readInLine
 
     let k = readIn fileLines
-    
+
+        
     printfn "%A" fileLines
     printfn "%A" k
