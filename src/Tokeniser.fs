a0cce66c9281ad372e60d60f9eb3765eab55e279
Matt Abdul-Rahim <Matthewar@users.noreply.github.com>
2017-03-07 22:57:56 +0000

    Better termination checks for pattern matches.
    Makes sure for most pattern matches that there is a terminating
    character after a match. (Forces a space or valid termination
    character).

--- src/Tokeniser.fs
+++ src/Tokeniser.fs
@@ -23,15 +23,18 @@ module Tokeniser =
    let isEndStatement (ch:char) =
       if ch = ';' then true else false
 
-   //Operators
+   //Useful constants
    let maths_operators = [| '+' ; '-' ; '*' ; '/' |]
    let operators  = [| '=' ;  '>' ; '<' ; ',' ; '(' ; ')' |]
    let operators2 = [| "<>" ; ">=" ; "<=" |]
+   let numbers = [| '0' .. '9' |]
+   let alpha = Array.concat [ [| 'a' .. 'z' |] ; [| 'A' .. 'Z' |] ]
+   let alphanum = Array.concat [ alpha ; numbers ]
 
    ///Check if a character terminates a name or number match
    let isValidStop (ch:char) =
       let validStopChars = Array.concat [ maths_operators ; operators ]
-      (Array.contains ch validStopChars) || (isWhitespace ch) || (isEndStatement ch)
+      Array.contains ch validStopChars || isWhitespace ch || isEndStatement ch
 
    ///Pattern match for operators
    ///Returns string of operator and remaining characters if successful
@@ -61,7 +64,6 @@ module Tokeniser =
    ///Pattern match for integers
    ///Returns integer and remaining characters if successful
    let (|NumMatch|_|) (str:char list) =
-      let numbers = [| '0' .. '9' |]
       ///Parse through character list extracting set of consecutive numbers
       let rec parse (outLst:char list) (inLst:char list) =
          match inLst with
@@ -85,7 +87,6 @@ module Tokeniser =
    ///Pattern match for floating point numbers
    ///Returns float and remaining characters if successful
    let (|FloatMatch|_|) (str:char list) =
-      let numbers = [| '0' .. '9' |]
       ///Parse through character list extracting set of consecutive numbers, must include a point
       let rec parse (outLst:char list) (inLst:char list) (point:bool) (goodFloat:bool) =
          match inLst with
@@ -95,7 +96,7 @@ module Tokeniser =
             else
                parse (ch :: outLst) rest point goodFloat
          | ch :: rest when (ch = '.') && (not point) -> parse (ch :: outLst) rest true false
-         | ch :: rest when goodFloat && (isWhitespace ch) -> Some(outLst,rest)
+         | ch :: rest when goodFloat && (isValidStop ch) -> Some(outLst,rest)
          | [] when goodFloat -> Some(outLst,[])
          | _ -> None
       ///Convert parsed number into usable numerical form
@@ -116,46 +117,67 @@ module Tokeniser =
    ///Pattern match for literals (strings encapsulated in quotation marks)
    ///Returns string and remaining characters if successful
    let (|LitMatch|_|) (str:char list) =
+      ///Process output of parser
+      let output (litLst:char list) (rest:char list) =
+         List.rev litLst
+         |> charListToString
+         |> fun x ->
+             if x.Length < 256 then (x,rest) else failwithf "Literal is too long"
       ///Parse through character list until find the terminating character (")
       let rec parse (outLst:char list) (inLst:char list) =
          match inLst with
          | ch :: rest when ch = '"' ->
-            List.rev outLst
-            |> charListToString
-            |> fun x ->
-                if x.Length < 256 then (x,rest) else failwithf "Literal is too long"
+            match rest with
+            | ch2 :: tail when isValidStop ch2 -> output outLst (ch2 :: tail)
+            | [] -> output outLst []
+            | _ -> failwithf "Error, invalid literal (need termination between final \" and next character)"
          | ch :: rest -> parse (ch :: outLst) rest
          | [] -> failwithf "Error, literal not finished"
       match str with
       | '"' :: rest -> Some(parse [] rest)
       | _ -> None
 
-   ///Pattern match for bytes, format: '<number>'B //?? Currently incorrect
+   ///Pattern match for bytes, format: <number>B
    ///Returns string and remaining characters if successful
    let (|ByteMatch|_|) (str:char list) =
+      ///Process output of parser
+      let output (litLst:char list) (rest:char list) =
+         litLst
+         |> List.rev
+         |> charListToString
+         |> int
+         |> fun x -> if x < 256 && x > 0 then Some(byte x,rest) else failwithf "Byte is outside range"
+      ///Parse through character list extracting set of consecutive numbers, ending in B
+      let rec parse (outLst:char list) (inLst:char list) =
+         match inLst with
+         | ch :: rest when Array.contains ch numbers -> parse (ch :: outLst) rest
+         | ch1 :: rest when ch1 = 'B' ->
+            match rest with
+            | ch2 :: tail when isValidStop ch2 -> output outLst (ch2 :: tail)
+            | [] -> output outLst []
+            | _ -> failwithf "Error, invalid byte (need termination between B and next character)"
+         | _ -> None
       match str with
-      | '\'' :: mid ->
-         match mid with
-         | byt :: '\'' :: 'B' :: rest when byt <> '\'' -> Some(byte(byt),rest)
-         | ch1 :: ch2 :: rest ->
-            printfn "Expected ' got %A" ch2
-            failwithf "Error"
-         | _ -> failwithf "Error, expected '"
+      | ch :: rest when Array.contains ch numbers -> parse [] rest
       | _ -> None
 
    ///Pattern match for booleans, IE True or False
    ///Returns bool and remaining characters if successful
    let (|BoolMatch|_|) (str:char list) =
+      ///Check for valid break after literal
+      let output (boolean:bool) (rest:char list) =
+         match rest with
+         | [] -> Some(boolean,rest)
+         | ch :: tail when isValidStop ch -> Some(boolean,ch::tail)
+         | _ -> None
       match str with
-      | 'T' :: 'r' :: 'u' :: 'e' :: rest -> Some(true,rest)
-      | 'F' :: 'a' :: 'l' :: 's' :: 'e' :: rest -> Some(false,rest)
+      | 'T' :: 'r' :: 'u' :: 'e' :: rest -> output true rest
+      | 'F' :: 'a' :: 'l' :: 's' :: 'e' :: rest -> output false rest
       | _ -> None
 
    ///Pattern match for names, alphanumeric strings that start with an alphabetic character
    ///Returns string and remaining characters if successful
    let (|NameMatch|_|) (str:char list) =
-      let alpha = Array.concat [ [| 'a' .. 'z' |] ; [| 'A' .. 'Z' |] ]
-      let alphanum = Array.concat [ alpha ; [| '0' .. '9' |] ]
       ///Pase through character list to collect alphanumeric word
       let rec parse (outLst:char list) (inLst:char list) =
          match inLst with
