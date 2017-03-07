879ebb4fabddc49f741dce22b8ee8ca7ee0f9f00
Matt Abdul-Rahim <Matthewar@users.noreply.github.com>
2017-03-07 13:48:52 +0000

    Continued work on select statement parsing.

--- src/AST.fs
+++ src/AST.fs
@@ -25,8 +25,8 @@ module AST =
       | Or
 
    type node =
-      | Branch of keyword * Children:node list
-      | Item of keyword * Child:node
+      | Branch of Name:node * Children:node list
+      | Item of Name:node * Child:node
       | Literal of Token.content
       | Key of keyword
       | Null
@@ -35,13 +35,28 @@ module AST =
       | Result of 'a
       | Error of string
 
-   let ReturnWrapper<'a> (func:'a->ReturnCode<'a>) =
-      let newFunc (data:ReturnCode<'a>) =
-         match data with
-         | Result(data) ->
-            func data
-         | error -> error
-      newFunc
+   let ReturnWrapper (func:(Tokeniser.tokens->ReturnCode<node*Tokeniser.tokens>)) (data:ReturnCode<node list*Tokeniser.tokens>) : ReturnCode<node list*Tokeniser.tokens> =
+      match data with
+      | Result(res) ->
+         snd res
+         |> func
+         |> fun x ->
+            match x with
+            | Result(nod,tLst) -> Result(((fst res) @ [nod]),tLst)
+            | Error(s) -> Error(s)
+      | Error(s) -> Error (s)
+
+   let OptionalReturnWrapper matchFunc data =
+      match data with
+      | Result(nLst,tLst) ->
+         match tLst with
+         | matchFunc(matchedResult) ->
+            match matchedResult with
+            | Result(nod,newTLst) ->
+               Result((nLst @ [nod]),newTLst)
+            | Error(s) -> Error(s)
+         | _ -> Result(nLst,tLst)
+      | Error(s) -> Error(s)
 
    let processTupleConsOnReturn (item:'a) (rest:ReturnCode<'a list*'b>) : ReturnCode<'a list*'b> =
       match rest with
@@ -58,48 +73,66 @@ module AST =
          let validFunctions = [| "AVG" ; "MAX" ; "MIN" ; "SUM" ; "ROUND" |]
          match lst with
          | Token.content.Name(funName) :: Token.content.Operator("(") :: Token.content.Name(colName) :: Token.content.Operator(")") :: rest when Array.contains funName validFunctions ->
-            [node.Item(keyword.Function,Literal(Token.content.Name(funName))) ; node.Item(keyword.Name,Literal(Token.content.Name(colName)))]
+            [node.Item(Key(Function),Literal(Token.content.Name(funName))) ; node.Item(Key(Name),Literal(Token.content.Name(colName)))]
             |> fun x -> Some(x,rest)
          | _ -> None
       let (|AliasMatch|_|) (lst:Tokeniser.tokens) =
          match lst with
          | Token.content.Name(colName) :: Token.content.Name("AS") :: Token.content.Literal(colAlias) :: rest ->
-            [node.Item(keyword.Name,Literal(Token.content.Name(colName))) ; node.Item(keyword.Alias,Literal(Token.content.Literal(colAlias)))]
+            [node.Item(Key(Name),Literal(Token.content.Name(colName))) ; node.Item(Key(Alias),Literal(Token.content.Literal(colAlias)))]
             |> fun x -> Some(x,rest)
          | _ -> None
       let rec parse (lst:Tokeniser.tokens) (nextColumn:bool) : ReturnCode<node list*Tokeniser.tokens> =
          match lst with
          | FunctionMatch (funLst,rest) when nextColumn ->
-            processTupleConsOnReturn (Branch(keyword.Function,funLst)) (parse rest false)
+            processTupleConsOnReturn (Branch(Key(Function),funLst)) (parse rest false)
          | AliasMatch (aliasLst,rest) when nextColumn ->
-            processTupleConsOnReturn (Branch(keyword.Alias,aliasLst)) (parse rest false)
+            processTupleConsOnReturn (Branch(Key(Alias),aliasLst)) (parse rest false)
          | Token.content.Name(name) :: rest when nextColumn ->
-            processTupleConsOnReturn (Item(keyword.Name,Literal(Token.content.Name(name)))) (parse rest false)
+            processTupleConsOnReturn (Item(Key(Name),Literal(Token.content.Name(name)))) (parse rest false)
          | Token.content.Operator(",") :: rest when not nextColumn -> parse rest true
          | item :: rest when nextColumn -> Error(sprintf "Expected wrapped column name, got %A" item)
          | rest -> Result([],rest)
       parse tokenList true
       |> fun x ->
          match x with
-         | Result(columnLst,rest) -> Result(node.Branch(keyword.Column,columnLst),rest)
+         | Result(columnLst,rest) -> Result(node.Branch(Key(Column),columnLst),rest)
          | Error(error) -> Error(error)
 
-   //let TableList (tokenList
+   let TableList (tokenList:Tokeniser.tokens) : ReturnCode<node*Tokeniser.tokens> =
+      let rec parse (lst:Tokeniser.tokens) (nextItem:bool) =
+          match lst with
+          | Token.content.Name(name) :: rest when nextItem ->
+             processTupleConsOnReturn (node.Literal(Token.content.Name(name))) (parse rest false)
+          | Token.content.Operator(op) :: rest when op = "," && not nextItem ->
+             parse rest true
+          | item :: rest when nextItem ->
+             Error(sprintf "Expected table name, got %A" item)
+          | rest ->
+             Result([],rest)
+      parse tokenList true
+      |> fun x ->
+         match x with
+         | Result(nLst,tLst) -> Result(node.Branch(Key(From),nLst),tLst)
+         | Error (s) -> Error(s)
 
    let (|BranchMatch|_|) (tokenList:Tokeniser.tokens) =
-      let output (key:keyword) (result:ReturnCode<node*Tokeniser.tokens>) =
+      let output (key:Key) (result:ReturnCode<node*Tokeniser.tokens>) =
          match result with
-         | Results(res) ->
-            (key,fst res,snd res)
-            |> fun x->
-               Some(ReturnCode<node*node list*Tokeniser.tokens>.Results(x))
-         | Error(s) ->
-            Some(ReturnCode<node*node list*Tokeniser.tokens>.Error(s))
+         | Result(res) -> Result(key,fst res,snd res)
+         | Error(s) -> Error(s)
       let selectParse (tokenList:Tokeniser.tokens) =
-         tokenList
-         |> ColumnWrappedList
-         //?? Need to change how it is piped, it returns something that should be in the body, pass rest to cons.
-         |> ReturnWrapper<node*Tokeniser.tokens> TableList
+         let (|ConditionsList|_|) (input:Tokeniser.tokens) =
+            match input with
+            | Token.content.Name("WHERE") :: rest -> Some
+            | _ -> None
+         Result([],tokenList)
+         |> ReturnWrapper ColumnWrappedList
+         |> ReturnWrapper TableList
+         |> OptionalReturnWrapper ConditionsList
+         |> OptionalReturnWrapper OrderList
+         |> OptionalReturnWrapper LimitItem
+         |> fun x -> Some(x)
          //Returns body:node * rest:Tokeniser.tokens
       let insertParse (tokenList:Tokeniser.tokens) =
          //Returns body:node * rest:Tokeniser.tokens
@@ -114,12 +147,12 @@ module AST =
       match tokenList with
       | item :: rest ->
          match item with
-         | Token.content.Name("SELECT") -> Some(output (Key(keyword.Select)) (selectParse rest))
-         | Token.content.Name("INSERT") -> Some(output (Key(keyword.Insert)) (insertParse rest))
-         | Token.content.Name("UPDATE") -> Some(output (Key(keyword.Update)) (updateParse rest))
-         | Token.content.Name("SET") -> Some(output (Key(keyword.Set)) (setParse rest))
-         | Token.content.Name("DECLARE") -> Some(output (Key(keyword.Declare)) (declareParse rest))
-         | Token.content.Name("DELETE") -> Some(output (Key(keyword.Delete)) (deleteParse rest))
+         | Token.content.Name("SELECT") -> Some(output (Key(Select)) (selectParse rest))
+         | Token.content.Name("INSERT") -> Some(output (Key(Insert)) (insertParse rest))
+         | Token.content.Name("UPDATE") -> Some(output (Key(Update)) (updateParse rest))
+         | Token.content.Name("SET") -> Some(output (Key(Set)) (setParse rest))
+         | Token.content.Name("DECLARE") -> Some(output (Key(Declare)) (declareParse rest))
+         | Token.content.Name("DELETE") -> Some(output (Key(Delete)) (deleteParse rest))
          | _ -> None;
       | _ -> None;
 
