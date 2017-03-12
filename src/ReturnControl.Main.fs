fb9126206121f91a15f7ad13a07e9eb62c80add4
Matt Abdul-Rahim <Matthewar@users.noreply.github.com>
2017-03-12 23:55:50 +0000

    Split apart files for usability.
    Files now named according to namespace and module.
    Removed types and control functions (wrapper IO control) to separate
    file.
    Placed return control methods into separate file, and separated parser
    specific ones.

---
+++ src/ReturnControl.Main.fs
@@ -0,0 +1,15 @@
+﻿namespace ReturnControl
+module Main =
+    type ReturnCode<'a> =
+        | Result of 'a
+        | Error of string
+
+    let UnwrapResultThrough func (from:ReturnCode<'a>) =
+        match from with
+        | Result(res) -> Result(func res)
+        | Error(str) -> Error(str)
+
+    let UnwrapResultInto func (from:ReturnCode<'a>) =
+        match from with
+        | Result(res) -> func res
+        | Error(str) -> Error(str)
