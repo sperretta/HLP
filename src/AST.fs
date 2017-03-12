38e0f88c54b11326f29f2d0d0c3d89e058c966bc
Matt Abdul-Rahim <Matthewar@users.noreply.github.com>
2017-03-12 12:01:03 +0000

    Work on insert statement.

--- src/AST.fs
+++ src/AST.fs
@@ -51,25 +51,74 @@ module AST =
         | Result(res) -> func res
         | Error(str) -> Error(str)
 
-    let ReturnWrapper func data : ReturnCode<node list*Tokeniser.tokens*Variable.Variable.typeContainer> =
-        let readInputSuccess (nodeList,tokenList,vars) =
-            tokenList
-            |> func vars
-            |> UnwrapResultThrough (fun (node,tLst,newVars) -> ((nodeList) @ [node]),tLst,newVars)
-        data
-        |> UnwrapResultInto readInputSuccess
+    type passThroughInputs = {nodeList:node list ; tokenList:Tokeniser.tokens ; varList:Variable.Variable.typeContainer ; number:int option}
 
-    let OptionalReturnWrapper (|MatchFunc|_|) (data:ReturnCode<node list*Tokeniser.tokens*Variable.Variable.typeContainer>) =
-        let processReturn (nodeList,tokenList,vars) =
+    let VarsInput = fun (nodeList,tokenList,vars) ->
+        let inputArgs = vars
+        let outputs = {nodeList=nodeList ; tokenList=tokenList ; varList=vars ; number = None}
+        (tokenList,inputArgs,outputs)
+    let VarsOutput constInputs =
+        let resultsFromFunc = fun (node,newTokenList,newVars) -> (node :: constInputs.nodeList),newTokenList,newVars
+        let passThroughAll = (constInputs.nodeList,constInputs.tokenList,constInputs.varList)
+        (resultsFromFunc,passThroughAll)
+    let VarsAndNumberInput = fun (nodeList,tokenList,vars,number:int option) ->
+        let inputArgs = vars,number.Value
+        let outputs = {nodeList=nodeList ; tokenList=tokenList ; varList=vars ; number = number}
+        (tokenList,inputArgs,outputs)
+    let VarsAndNumberOutput constInputs =
+        let resultsFromFunc = fun (node,newTokenList,newVars,newNumber) -> (node :: constInputs.nodeList),newTokenList,newVars,Some(newNumber)
+        let passThroughAll = (constInputs.nodeList,constInputs.tokenList,constInputs.varList,constInputs.number)
+        (resultsFromFunc,passThroughAll)
+    let NumberInput = fun (nodeList,tokenList,vars,number:int option) ->
+        let inputArgs = number.Value
+        let outputs = {nodeList=nodeList ; tokenList=tokenList ; varList=vars ; number = number}
+        (tokenList,inputArgs,outputs)
+    let NumberOutput constInputs =
+        let resultsFromFunc = fun (node,newTokenList,newNumber) -> (node :: constInputs.nodeList),newTokenList,constInputs.varList,Some(newNumber)
+        let passThroughAll = (constInputs.nodeList,constInputs.tokenList,constInputs.varList,constInputs.number)
+        (resultsFromFunc,passThroughAll)
+    let NoVarsInput = fun (nodeList,tokenList,vars) ->
+        let inputArgs = ()
+        let outputs = {nodeList=nodeList ; tokenList=tokenList ; varList=vars ; number = None}
+        (tokenList,inputArgs,outputs)
+    let NoVarsOutput constInputs =
+        let resultsFromFunc = fun (node,newTokenList) -> (node :: constInputs.nodeList),newTokenList,constInputs.varList
+        let passThroughAll = (constInputs.nodeList,constInputs.tokenList,constInputs.varList)
+        (resultsFromFunc,passThroughAll)
+
+    let getTokenList (tokenList,_,_) = tokenList
+    let getInputArgs (_,inputArgs,_) = inputArgs
+    let getOutputConsts (_,_,outputConsts) = outputConsts
+
+    let ReturnWrapper getInputInfoFrom getOutputFuncFrom func data =
+        let runFunc unwrappedData =
+            let completedInputInfo = getInputInfoFrom unwrappedData
+            let tokenList = getTokenList completedInputInfo
+            let inputArgs = getInputArgs completedInputInfo
+            let outputProcesses = getOutputFuncFrom (getOutputConsts completedInputInfo)
+            let processFuncOutputs = fst outputProcesses
+            func inputArgs tokenList
+            |> UnwrapResultThrough processFuncOutputs
+        data
+        |> UnwrapResultInto runFunc
+
+    let OptionalReturnWrapper getInputInfoFrom getOutputFuncFrom (|MatchFunc|_|) data =
+        let processReturn unwrappedData =
+            let completedInputInfo = getInputInfoFrom unwrappedData
+            let tokenList = getTokenList completedInputInfo
+            let inputArgs = getInputArgs completedInputInfo
+            let outputProcesses = getOutputFuncFrom (getOutputConsts completedInputInfo)
+            let processFuncOutputs = fst outputProcesses
+            let ignoreFuncOutput = snd outputProcesses
             match tokenList with
-            | MatchFunc vars matchedResult ->
+            | MatchFunc inputArgs matchedResult ->
                 matchedResult
-                |> UnwrapResultThrough (fun (node,newTokenList,newVars) -> (nodeList @ [node]),newTokenList,newVars)
-            | _ -> Result(nodeList,tokenList,vars)
+                |> UnwrapResultThrough processFuncOutputs
+            | _ -> Result(ignoreFuncOutput)
         data
         |> UnwrapResultInto processReturn
 
-    let ColumnWrappedList (vars:Variable.Variable.typeContainer) (tokenList:Tokeniser.tokens) : ReturnCode<node*Tokeniser.tokens*Variable.Variable.typeContainer> =
+    let ColumnWrappedList () (tokenList:Tokeniser.tokens) : ReturnCode<node*Tokeniser.tokens> =
         let (|FunctionMatch|_|) (lst:Tokeniser.tokens) =
             let validFunctions = [| "AVG" ; "MAX" ; "MIN" ; "SUM" ; "ROUND" |]
             match lst with
@@ -92,17 +141,7 @@ module AST =
             | item :: rest when nextColumn -> Error(sprintf "Expected wrapped column name, got %A" item)
             | rest -> Result(outLst,rest)
         parse [] tokenList true
-        |> UnwrapResultThrough (fun (columnList,rest) -> Branch(Key(Column),(List.rev columnList)),rest,vars)
-
-    let TableList (vars:Variable.Variable.typeContainer) (tokenList:Tokeniser.tokens) : ReturnCode<node*Tokeniser.tokens*Variable.Variable.typeContainer> =
-        let rec parse (outLst:node list) (lst:Tokeniser.tokens) (nextItem:bool) =
-             match lst with
-             | Token.content.Name(name) :: rest when nextItem -> parse (Literal(Token.content.Name(name)) :: outLst) rest false
-             | Token.content.Operator(",") :: rest when not nextItem -> parse outLst rest true
-             | item :: rest when nextItem -> Error(sprintf "Expected table name, got %A" item)
-             | rest -> Result(outLst,rest)
-        parse [] tokenList true
-        |> UnwrapResultThrough (fun (nodeList,tokenList) -> Branch(Key(From),(List.rev nodeList)),tokenList,vars)
+        |> UnwrapResultThrough (fun (columnList,rest) -> Branch(Key(Column),(List.rev columnList)),rest)
 
     let ValueItem (vars:Variable.Variable.typeContainer) (input:Tokeniser.tokens) : ReturnCode<node*Tokeniser.tokens> =
         let output (tLst:Tokeniser.tokens) (nod:node) =
@@ -117,11 +156,12 @@ module AST =
         | Token.content.Name(varName) :: rest when vars.ContainsKey varName ->
             Item(Key(Variable),Literal(Token.content.Name(varName)))
             |> output rest
+        | Token.content.Name(varName) :: rest -> Error(sprintf "Could not find declaration for variable %s" varName)
         | tok :: _ -> Error(sprintf "Invalid value %A" tok)
         | [] -> Error("Run out of tokens when expected value")
         
 
-    let ConditionsList (vars:Variable.Variable.typeContainer) (input:Tokeniser.tokens) : (ReturnCode<node*Tokeniser.tokens*Variable.Variable.typeContainer> option) =
+    let ConditionsList (vars:Variable.Variable.typeContainer) (input:Tokeniser.tokens) : (ReturnCode<node*Tokeniser.tokens> option) =
         let (|ConditionMatch|) (tokenList:Tokeniser.tokens) =
             let isCompareOp str = 
                 let compareOperators = [| "=" ; "<>" ; "<" ; ">" ; "<=" ; ">=" |]
@@ -151,7 +191,7 @@ module AST =
             | rest -> Result(outLst,rest)
         let parseExtraConditions (nodeBody,newTLst) =
             parse [Branch(Key(Condition),nodeBody)] newTLst
-            |> UnwrapResultThrough (fun (whereBody,tailTokenList) -> Branch(Key(Where),List.rev whereBody),tailTokenList,vars)
+            |> UnwrapResultThrough (fun (whereBody,tailTokenList) -> Branch(Key(Where),List.rev whereBody),tailTokenList)
         match input with
         | Token.content.Name("WHERE") :: rest ->
             rest
@@ -159,7 +199,7 @@ module AST =
             |> fun x -> Some(x)
         | _ -> None
 
-    let OrderList (vars:Variable.Variable.typeContainer) (input:Tokeniser.tokens) =
+    let OrderList () (input:Tokeniser.tokens) =
         let rec parse (outLst:node list) (lst:Tokeniser.tokens) (nextItem:bool) =
             match lst with
             | Token.content.Name(columnName) :: Token.content.Name(direction) :: rest when nextItem ->
@@ -178,18 +218,18 @@ module AST =
         match input with
         | Token.content.Name("ORDER") :: Token.content.Name("BY") :: tokenList ->
             parse [] tokenList true
-            |> UnwrapResultThrough (fun (nodeList,newTokenList) -> Branch(Key(Order),List.rev nodeList),newTokenList,vars)
+            |> UnwrapResultThrough (fun (nodeList,newTokenList) -> Branch(Key(Order),List.rev nodeList),newTokenList)
             |> fun x -> Some(x)
         | _ -> None
 
-    let TableName (vars:Variable.Variable.typeContainer) (tokenList:Tokeniser.tokens) =
+    let TableName () (tokenList:Tokeniser.tokens) =
         match tokenList with
         | Token.content.Name(tableName) :: rest ->
-            Result(Item(Key(Into),Literal(Token.content.Name(tableName))),rest,vars)
+            Result(Item(Key(Into),Literal(Token.content.Name(tableName))),rest)
         | item :: rest -> Error(sprintf "Expected table name, got %A" item)
         | [] -> Error("Expected table name, ran out of tokens")
 
-    let ColumnNameList (vars:Variable.Variable.typeContainer) (tokenList:Tokeniser.tokens) =
+    let ColumnNameList () (tokenList:Tokeniser.tokens) =
         let rec parse (outLst:node list) (lst:Tokeniser.tokens) (nextItem:bool) (numColumns:int) =
             match lst with
             | Token.content.Name(name) :: rest when nextItem -> parse (Item(Key(Name),Literal(Token.content.Name(name))) :: outLst) rest false (numColumns+1)
@@ -200,11 +240,49 @@ module AST =
         match tokenList with
         | Token.content.Operator("(") :: rest ->
             parse [] rest true 0
-            |> UnwrapResultThrough (fun (nodeList,tokenList,numColumns) -> Branch(Key(Column),List.rev nodeList),tokenList,vars,numColumns)
+            |> UnwrapResultThrough (fun (nodeList,tokenList,numColumns) -> Branch(Key(Column),List.rev nodeList),tokenList,numColumns)
             |> fun x -> Some(x)
         | _ -> None
         
-    let TableList (vars:Variable.Variable.typeContainer) (tokenList:Tokeniser.tokens) : ReturnCode<node*Tokeniser.tokens*Variable.Variable.typeContainer> =
+    let ValueList (vars:Variable.Variable.typeContainer,numColumns:int option) (tokenList:Tokeniser.tokens) : ReturnCode<node*Tokeniser.tokens> =
+        let (|ValueMatch|) = ValueItem
+        let rec parse (outLst:node list) (lst:Tokeniser.tokens) (nextItem:bool) (numValues:int) =
+            match lst with
+            | ValueMatch vars matchedValue when nextItem ->
+                matchedValue
+                |> UnwrapResultInto (fun (valNode,tokenList) -> parse (Item(Key(Value),valNode) :: outLst) tokenList false (numValues+1))
+            | Token.content.Operator(",") :: rest when not nextItem -> parse outLst rest true numValues
+            | item :: rest when nextItem -> Error(sprintf "Expected value, got %A" item)
+            | Token.content.Operator(")") :: rest when not nextItem -> Result(outLst,rest,numValues)
+            | item :: rest when not nextItem -> Error(sprintf "Expected , or ), got %A" item)
+            | [] when nextItem -> Error("Expected value, ran out of tokens")
+            | [] when not nextItem -> Error("Expected , or ), ran out of tokens")
+            | rest -> Error("Unknown error on sequence")
+        let compareValues num =
+            match numColumns with
+            | Some(numCol) when numCol = num -> true
+            | None -> true
+            | _ -> false
+        match tokenList with
+        | Token.content.Name("VALUES") :: Token.content.Operator("(") :: rest ->
+            parse [] rest true 0
+            |> 
+                
+
+
+
+             | Token.content.Name(name) :: rest when nextItem -> parse (Literal(Token.content.Name(name)) :: outLst) rest false
+             | Token.content.Operator(",") :: rest when not nextItem -> parse outLst rest true
+             | item :: rest when nextItem -> Error(sprintf "Expected table name, got %A" item)
+             | rest -> Result(outLst,rest)
+        match tokenList with
+        | Token.content.Name("FROM") :: rest ->
+            parse [] rest true
+            |> UnwrapResultThrough (fun (nodeList,tokenList) -> Branch(Key(From),(List.rev nodeList)),tokenList)
+        | item :: rest -> Error(sprintf "Expected FROM, got %A" item)
+        | [] -> Error("Expected FROM, ran out of tokens")
+
+    let TableList () (tokenList:Tokeniser.tokens) : ReturnCode<node*Tokeniser.tokens> =
         let rec parse (outLst:node list) (lst:Tokeniser.tokens) (nextItem:bool) =
              match lst with
              | Token.content.Name(name) :: rest when nextItem -> parse (Literal(Token.content.Name(name)) :: outLst) rest false
@@ -214,18 +292,18 @@ module AST =
         match tokenList with
         | Token.content.Name("FROM") :: rest ->
             parse [] rest true
-            |> UnwrapResultThrough (fun (nodeList,tokenList) -> Branch(Key(From),(List.rev nodeList)),tokenList,vars)
+            |> UnwrapResultThrough (fun (nodeList,tokenList) -> Branch(Key(From),(List.rev nodeList)),tokenList)
         | item :: rest -> Error(sprintf "Expected FROM, got %A" item)
         | [] -> Error("Expected FROM, ran out of tokens")
 
-    let LimitItem (var:Variable.Variable.typeContainer) (input:Tokeniser.tokens) =
+    let LimitItem () (input:Tokeniser.tokens) =
         let (|OffsetMatch|_|) (tokenList:Tokeniser.tokens) =
             match tokenList with
             | Token.content.Name("OFFSET") :: Token.Value(Token.value.Integer(offsetVal)) :: rest -> Some(Result(offsetVal,rest))
             | Token.content.Name("OFFSET") :: rest -> Some(Error("Expected offset value (integer)"))
             | _ -> None
         let successOutput (tokenList:Tokeniser.tokens) (nodeList:node list) =
-            (Branch(Key(Limit),nodeList),tokenList,var)
+            (Branch(Key(Limit),nodeList),tokenList)
         let checkForOffset (tokenList:Tokeniser.tokens) (limitVal:int) =
             let limitItem = Item(Key(Value),Literal(Token.content.Value(Token.value.Integer(limitVal))))
             let outputWithOffset (offsetVal:int,remainList:Tokeniser.tokens) =
@@ -251,16 +329,18 @@ module AST =
             |> UnwrapResultThrough (fun (nodeList,tokenList,vars) -> (Key(key),nodeList,tokenList,vars))
         let selectParse (tokenList:Tokeniser.tokens) =
             Result([],tokenList,vars)
-            |> ReturnWrapper ColumnWrappedList
-            |> ReturnWrapper TableList
-            |> OptionalReturnWrapper ConditionsList
-            |> OptionalReturnWrapper OrderList
-            |> OptionalReturnWrapper LimitItem
+            |> ReturnWrapper NoVarsInput NoVarsOutput ColumnWrappedList
+            |> ReturnWrapper NoVarsInput NoVarsOutput TableList
+            |> OptionalReturnWrapper VarsInput NoVarsOutput ConditionsList
+            |> OptionalReturnWrapper NoVarsInput NoVarsOutput OrderList
+            |> OptionalReturnWrapper NoVarsInput NoVarsOutput LimitItem
+            |> UnwrapResultThrough (fun (nodeList,tokenList,varMap) -> List.rev nodeList,tokenList,varMap)
         let insertParse (tokenList:Tokeniser.tokens) =
             Result([],tokenList,vars)
-            |> ReturnWrapper TableName
-            |> OptionalReturnWrapper ColumnNameList
-            |> ReturnWrapper ValueList
+            |> ReturnWrapper NoVarsInput NoVarsOutput TableName
+            |> OptionalReturnWrapper NoVarsInput NumberOutput ColumnNameList
+            |> ReturnWrapper VarsAndNumberInput NoVarsOutput ValueList
+            |> UnwrapResultThrough (fun (nodeList,tokenList,varMap) -> List.rev nodeList,tokenList,varMap)
         let updateParse (tokenList:Tokeniser.tokens) =
             Result([],tokenList,vars)
         let setParse (tokenList:Tokeniser.tokens) =
