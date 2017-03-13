3925d24532ed8cf33cfde798fa4d6864d716d5db
Matt Abdul-Rahim <Matthewar@users.noreply.github.com>
2017-03-13 02:03:06 +0000

    Started work on parsing AST (Execution Engine) #21

---
+++ src/ExecutionEngine.Parse.fs
@@ -0,0 +1,14 @@
+﻿namespace ExecutionEngine
+module Parse =
+    open Parser.ControlAndTypes
+    let runThroughTree (tree:node list) =
+        let rec parse (branch:node list) =
+            match branch with
+            | Branch(Key(Select),children) :: rest
+            | Branch(Key(Insert),children) :: rest
+            | Branch(Key(Update),children) :: rest
+            | Branch(Key(Set),children) :: rest
+            | Branch(Key(Declare),children) :: rest
+            | Branch(Key(Delete),children) :: rest
+            | Branch(Key(Create),children) :: rest
+            | item :: _ -> Error(sprintf "Unrecognised node in top level parse" item)
