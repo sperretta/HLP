f9a3ce061c828acef48ca9bd35fd116661968fe1
Matt Abdul-Rahim <Matthewar@users.noreply.github.com>
2017-03-13 21:52:44 +0000

    Further work on execution engine parser.

--- src/ExecutionEngine.Parse.fs
+++ src/ExecutionEngine.Parse.fs
@@ -1,14 +1,51 @@
 ﻿namespace ExecutionEngine
 module Parse =
     open Parser.ControlAndTypes
+    open ReturnControl.Main
+
+    let interpetSelect branches variables =
+        open Select
+    let interpetInsert branches variables =
+        open Insert
+    let interpetUpdate branches variables =
+        open Update
+    let interpetSet branches variables =
+        open Set
+    let interpetDeclare branches variables =
+        open Declare
+    let interpetDelete branches variables =
+        open Delete
+    let interpetCreate branches =
+        open Create
+
     let runThroughTree (tree:node list) =
-        let rec parse (branch:node list) =
-            match branch with
-            | Branch(Key(Select),children) :: rest
-            | Branch(Key(Insert),children) :: rest
-            | Branch(Key(Update),children) :: rest
-            | Branch(Key(Set),children) :: rest
-            | Branch(Key(Declare),children) :: rest
-            | Branch(Key(Delete),children) :: rest
-            | Branch(Key(Create),children) :: rest
-            | item :: _ -> Error(sprintf "Unrecognised node in top level parse" item)
+        let rec parse (branches:node list) (varMap:Variable.Variable.contentContainer) (returnTableList:returnTableListType) = //Returntabletype list =
+            match branches with
+            | Branch(Key(Select),children) :: rest ->
+                interpretSelect children varMap
+                |> UnwrapResultInto (fun readTable -> parse rest varMap (readTable :: returnTableList))
+            | Branch(Key(Insert),children) :: rest ->
+                interpretInsert children varMap
+                |> UnwrapResultInto (fun () -> parse rest varMap returnTableList)
+            | Branch(Key(Update),children) :: rest ->
+                interpretUpdate children varMap
+                |> UnwrapResultInto (fun () -> parse rest varMap returnTableList)
+            | Branch(Key(Set),children) :: rest ->
+                interpretSet children varMap
+                |> UnwrapResultInto (fun newVarMap -> parse rest newVarMap returnTableList)
+            | Branch(Key(Declare),children) :: rest ->
+                interpretDeclare children varMap
+                |> UnwrapResultInto (fun newVarMap -> parse rest newVarMap returnTableList)
+            | Branch(Key(Delete),children) :: rest ->
+                interpretDelete children varMap
+                |> UnwrapResultInto (fun () -> parse rest varMap returnTableList)
+            | Branch(Key(Create),children) :: rest ->
+                interpretCreate children
+                |> UnwrapResultInto (fun () -> parse rest varMap returnTableList)
+            | item :: _ -> Error(sprintf "Unrecognised node %A in top level parse" item)
+            | [] -> Result(varMap,returnTableList)
+        let convertToOutput (varMap,returnTableList) =
+            //Convert var map to strings, reverse table list
+            (varMap,List.rev returnTableList)
+        parse tree Map.empty []
+        |> UnwrapResultThrough convertToOutput
---
+++ src/Variable.fs
@@ -0,0 +1,32 @@
+﻿namespace Variable
+module Variable =
+    type varType =
+        | Integer
+        | Float
+        | Boolean
+        | Byte
+        | String
+
+    type varContent =
+        | Integer of int
+        | Float of float
+        | Boolean of bool
+        | Byte of byte
+        | String of string
+
+    type typeContainer = Map<string,varType>
+
+    type contentsContainer = Map<string,varType*varContent>
+
+    let validTypes =
+        [
+        "string" , varType.String ;
+        "byte" , varType.Byte ;
+        "int" , varType.Integer ;
+        "float" , varType.Float ;
+        "boolean" , varType.Boolean
+        ]
+        |> Map.ofList
+
+    let isValidVarType (name:string) =
+        Map.containsKey name validTypes
