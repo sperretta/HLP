bed4c8494dfb9104e06db83965e3b23dcc3b6816
Sigrid Passano Hellan <sighellan@gmail.com>
2017-03-14 15:50:52 +0000

    now takes input from text box and prints in to output box.

--- application/src/fs/Main.fs
+++ application/src/fs/Main.fs
@@ -9,42 +9,27 @@ open Fable.Import.Browser
 
 
 let main () =
-    (*let incCountPrint counter content =
-        counter <- counter + 1
-        content.innerHTML <- string counter *)
-    let GreetingWords = "Hello world!"
     let mutable myCounter = 0
     myCounter <- 1
     printfn "Starting..."
     let editId = getById<Fable.Import.Browser.HTMLTextAreaElement> "code"
     printfn "Creating editor"
     let cmEditor = App.CodeMirrorImports.CodeMirror.fromTextArea(editId, initOptions)
-    //let myTextArea = getById<Fable.Import.Browser.HTMLTextAreaElement> "panel"
-    //let cmEditor2 = App.CodeMirrorImports.CodeMirror.fromTextArea(myTextArea, initOptions)
-    //cmEditor2.setValue "hei"
-    //cmEditor2.setOption ("ReadOnly", "True")
     let content = Browser.document.getElementById "myText"
-    let button = Browser.document.getElementById("mySlide") :?> HTMLInputElement
-    
-    //myButton.innerHTML <- "New button"
-    let incCountPrint () =
-        myCounter <- myCounter + 1
-        console.log("Counter incremented")
-        content.innerHTML <- string myCounter
-    content.innerHTML <- (sprintf "<h1>%s</h1>" GreetingWords)
-    content.innerHTML <- content.innerHTML + "\nHello"
-    content.innerHTML <- content.innerHTML + "\n" + (string (3+2))
-    incCountPrint()
-    incCountPrint()
-    button.addEventListener_change(fun _ -> incCountPrint(); box())
-    //button.onclick = incCountPrint()
+    let slider = Browser.document.getElementById("mySlide") :?> HTMLInputElement
+    //let button = Browser.document.getElementById("myOtherButton") :?> HTMLInputElement
+    let incCountPrint () = // Change here to start actual program. 
+        myCounter <- myCounter + 1 // Have parser go through input, and then print to
+        console.log("Counter incremented") // the content box.
+        content.innerHTML <- ("    " + string myCounter + "<br />")
+        content.innerHTML <- content.innerHTML + string (cmEditor.getValue())
+    slider.addEventListener_change(fun _ -> incCountPrint(); box())
     printfn "Setting editor value"
     cmEditor.setValue " abc def *** //comment"
-    //printfn "Line tokens: %A" (cmEditor.getLineTokens 0)
+    printfn "Line tokens: %A" (cmEditor.getLineTokens 3)
     console.log("It's working!")
-    //let width = getElementById("width") : Fable.Import.Browser.HTMLInputElement
+    printfn "Hallo %A" (cmEditor.getValue())
     //render()
-    //cmEditor.setValue ""
     printfn "Main code finished"
     
 main()
