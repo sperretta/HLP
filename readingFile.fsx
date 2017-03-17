6eaacf5934b39729f78c1f12b1ac23f3709a25ef
Sigrid Passano Hellan <sighellan@gmail.com>
2017-03-17 18:07:40 +0000

    Now reads in data from file to a full database.

--- readingFile.fsx
+++ readingFile.fsx
@@ -1,14 +1,59 @@
 ﻿    open System.IO
     open System
-    type myData = | String of Option<string> | Int of Option<int> | Float of Option<float>                // To be extended when the wanted data types have been decided
-                  | Byte of Option<byte>     | Bool of Option<bool>
-    type IList = INode of ParName: string * Value: myData * Prev : ref<IList> * Tl : ref<IList> | INilLow // A node has the parameter name (column name), the parameter value
-                                                                                                          // and links to the previous and next nodes
-    type topList = Node of lowList : ref<IList> * Tl : ref<topList> | INilTop                             // The nodes of the top-level list have refs to low-level lists.
-    type data = ref<topList>                                                                              // Holds all the data in the table
-    type TableList = TableNode of topList : ref<topList> * TableName : string * Tl : ref<TableList> | INilTable // Holds all the tables in the database.
 
+    type boxData = | String of Option<string>  // To be extended when the wanted data types have been decided
+                   | Int of Option<int> 
+                   | Float of Option<float>                
+                   | Byte of Option<byte>     
+                   | Bool of Option<bool>
 
+    // A node has the parameter name (column name), the parameter value and links to the previous and next nodes
+    type boxList = BoxNode of ParName: string * Value: boxData * Prev : ref<boxList> * Tl : ref<boxList> | INilBox 
+    type row = ref<boxList>                                                                                       
+    // The nodes of the table-level list (rowList) have refs to the row-level lists (boxList).
+    type rowList = RowNode of BoxList : ref<boxList> * Tl : ref<rowList> | INilRow                            
+    type table = ref<rowList>  // Holds all the data in the table
+    // Holds all the tables in the database.
+    // A node has a table, a table name, a list with column names and types, and a tail pointing to the next table in the database.
+    type tableList = TableNode of topList : ref<rowList> * TableName : string * Columns : (string * boxData) list * Tl : ref<tableList> | INilTable 
+    type database = ref<tableList>
+
+    // Get the tail from a table (entry after last row)
+    let rec rowListLast thisList : table =
+        match !thisList with
+        | INilRow -> thisList
+        | RowNode (_, tail) -> rowListLast tail
+
+    // Get the first row from a table
+    let rowListFirstRow (thisList : table) =
+        match !thisList with
+        | INilRow -> None
+        | RowNode (row, _) -> Some row
+
+    // Get a specific table from a database
+    let rec chooseTable thisDatabase tableName =
+        match !thisDatabase with
+        | INilTable -> None
+        | TableNode (thisTable, thisName, _, _) when thisName = tableName -> Some thisTable
+        | TableNode (_, _, _, otherTables) -> chooseTable otherTables tableName
+
+    type ReturnCode<'a> = // From Matt
+        | Result of 'a
+        | Error of string
+
+    let UnwrapResultThrough func (from:ReturnCode<'a>) = // From Matt
+        match from with
+        | Result(res) -> Result(func res)
+        | Error(str) -> Error(str)
+
+    let UnwrapResultInto func (from:ReturnCode<'a>) = // From Matt
+        match from with
+        | Result(res) -> func res
+        | Error(str) -> Error(str)
+
+////////////////////////////////////////////////
+
+(*
     let head1  = ref INilLow
     let tail1  = ref INilLow
     let test2 = ref INilLow
@@ -29,8 +74,8 @@
     let myTopList = (Node (test1, tmpT))
     tmpT := Node (test3, tailT)
     myTopList
-
-    let helperFunc list f F caller emes=
+*)
+    let helperStrProcess list f F caller emes=
         match list with
         | "Some" :: c :: tail -> (c |> f |> Some |> F) :: caller tail
         | "None" :: tail -> F None :: caller tail
@@ -39,20 +84,14 @@
 
     let rec strProcess = function
         | [] -> []
-        | a :: tail when a = "String" -> helperFunc tail (fun x -> x) String strProcess a
-        | a :: tail when a = "Int"    -> helperFunc tail int Int strProcess a
-        | a :: tail when a = "Float"  -> helperFunc tail float Float strProcess a
-        | a :: tail when a = "Byte"   -> helperFunc tail byte Byte strProcess a
-        | a :: tail when a = "Bool"   -> helperFunc tail System.Convert.ToBoolean Bool strProcess a
+        | a :: tail when a = "String" -> helperStrProcess tail (fun x -> x) String strProcess a
+        | a :: tail when a = "Int"    -> helperStrProcess tail int Int strProcess a
+        | a :: tail when a = "Float"  -> helperStrProcess tail float Float strProcess a
+        | a :: tail when a = "Byte"   -> helperStrProcess tail byte Byte strProcess a
+        | a :: tail when a = "Bool"   -> helperStrProcess tail System.Convert.ToBoolean Bool strProcess a
         | e :: tail                   -> (Some >> String) ("error: " + e) :: (strProcess tail)
          
-    let exCol = ["Names"; "String"; "ID"; "Int"]
-    let exData = [String (Some "Hedda"); Int (Some 1)]
-    let exDataMult = [[String (Some "Hedda"); Int (Some 1)];[String (Some "Nora"); Int (Some 6)]]
-    let badData = [[String (Some "Hedvig"); Float (Some 3.2)]]
-
-       
-    let checkType colType inpNext = 
+    let compStringType colType inpNext = 
         match inpNext with
         | String a -> colType = "String"
         | Int a    -> colType = "Int"
@@ -60,45 +99,31 @@
         | Byte a   -> colType = "Byte"
         | Bool a   -> colType = "Bool"
 
-
     let rec matchInputListsRec columns inpData prevNode nextNode =
         match (columns,inpData) with 
         | ([],[]) -> ()
-        | (colName :: colType :: colTail, inpNext :: inpTail) when checkType colType inpNext->
-            let newNode = ref INilLow
-            nextNode := (INode (colName, inpNext, prevNode, newNode))
+        | (colName :: colType :: colTail, inpNext :: inpTail) when compStringType colType inpNext->
+            let newNode = ref INilBox
+            nextNode := (BoxNode (colName, inpNext, prevNode, newNode))
             matchInputListsRec colTail inpTail nextNode newNode
         | _ -> printfn "Error: %A" (columns,inpData)
     
     let matchInputLists columns inpData =
-        let firstHead = ref INilLow
-        let nextNode  = ref INilLow
+        let firstHead = ref INilBox
+        let nextNode  = ref INilBox
         matchInputListsRec columns inpData firstHead nextNode
         nextNode
-    matchInputLists exCol exData
 
     let listBuilder acc lowList =
-        let newNode = ref INilTop
-        acc := Node (lowList, newNode)
+        let newNode = ref INilRow
+        acc := RowNode (lowList, newNode)
         newNode
 
     let buildData columns inpData = 
         let tmp = List.map (matchInputLists columns) inpData
-        let firstHead = ref INilTop
+        let firstHead = ref INilRow
         List.fold listBuilder firstHead tmp |> ignore
         firstHead
-    buildData exCol exDataMult
-    buildData exCol badData
-
-
-    // Tests
-    strProcess ["String"; "Some"; "Orlando"; "Int"; "Some"; "45"]
-    strProcess ["Byte"; "Some"; "0xF3"; "Byte"; "None"]
-    strProcess ["Bool"; "Some"; "True"; "Bool"; "None"]
-    strProcess ["Float"; "Some"; "3.1415926"; "Float"; "None"]
-    strProcess ["String"; "some"; "hei"]
-    strProcess ["Strange"; "Some"; "Dragon"]
-
 
     let rec strBuilder = function
         | [] -> []
@@ -133,34 +158,15 @@
     let readIn mySeq =
         mySeq |> Seq.toList |> List.map readInLine
 
-    let myPath  = @"C:\Users\Sigrid\Documents\Visual Studio 2015\HLP\dataFile.txt"
-    let outPath = @"C:\Users\Sigrid\Documents\Visual Studio 2015\HLP\outFile.txt"
-    let someStruct = @"C:\Users\Sigrid\Documents\Visual Studio 2015\HLP\someStructureDataFile.txt"
-    let structPath = @"C:\Users\Sigrid\Documents\Visual Studio 2015\HLP\structuredDataFile.txt"
-
     let readFile pathString = IO.File.ReadLines pathString
-
-    let fileLines = readFile myPath
-
     let ReadInData myPath = 
         myPath |> IO.File.ReadLines |> readIn
-
-    let k = readIn fileLines
-    let j = ReadInData myPath
 
     let readTable inpStrings =
         let splitStrings = List.map cleanLine inpStrings
         let colNames = List.head splitStrings
         let colData = List.tail inpStrings |> List.map readInLine
         buildData colNames colData
-
-    let exCol2 = ["Names"; "String"; "ID"; "Int"; "Credit"; "Float"; "Hex"; "Byte"; "Member"; "Bool"]
-    let testSeparate = ["Names String ID Int";"String Some Orlando Int Some 45";"String Some Rebecca Int Some 42"]
-    readTable testSeparate
-    buildData exCol2 j
-
-    let testMultiple = ["TABLE";"Names String ID Int";"String Some Orlando Int Some 45";"String Some Rebecca Int Some 42"; "TABLE"; "Subject String"; "String Some Math"; "String Some Music"]
-
 
     let rec allInOneTable inpStrings rowList =
         match inpStrings with
@@ -169,19 +175,144 @@
         | a :: tail -> allInOneTable tail (a :: rowList)
 
     let rec wrapper inpStrings tableList =
-        let (table, strings) = allInOneTable inpStrings []
-        match strings with
+        let (table, otherStrings) = allInOneTable inpStrings []
+        match otherStrings with
         | [] -> table :: tableList 
         | "TABLE" :: tail -> wrapper tail (table::tableList)
-
-    wrapper ["TABLE"; "User String"; "X Y"; "Z W"; "TABLE"; "ID Int"; "X Y"; "W Z"] []
 
     let sortIntoMultiple inpStrings = 
         let tableLists = []
         match inpStrings with 
         | a :: tail when a = "TABLE" -> List.rev (wrapper tail [])
 
+    (*
+    let rec stringsOneTable inpStrings rowList =
+        match inpStrings with
+        | [] -> (List.rev rowList,[])
+        | a  
+
+    
+    let rec sortIntoMultipleRev inpList =
+        let (table, otherStrings) = allInOneTable inpList []
+        match otherStrings with
+        | [] -> []
+        | a :: tableName :: tail when a = "TABLE" -> (tableName, table) :: sortIntoMultipleRev tail *)
+
+    let rec sortIntoMultipleRev inpList =
+        let (table, otherStrings) = allInOneTable inpList []
+        match inpList with
+        | [] -> []
+        | a :: tableName :: tail when a = "TABLE" -> 
+            let (table, otherStrings) = allInOneTable tail [] 
+            (tableName, table) :: sortIntoMultipleRev otherStrings
+        | _ -> printfn "Error!" |> fun () -> []
+
+    let splitIntoThreeLists inpList =
+        List.foldBack (fun (tabName, typeString :: dataString) (nameAcc, typeAcc, dataAcc) -> 
+            (tabName :: nameAcc, typeString :: typeAcc, (typeString :: dataString) :: dataAcc) ) inpList ([], [], [])
+
+    let rec getTypes = function
+        | [] -> []
+        | colName :: colType :: tail -> (colName, colType :: ["None"] |> strProcess |> List.head) :: getTypes tail
+
+    (*
+    let rec allInOneTableTwo inpStrings rowList =
+        match inpStrings with
+        | [] -> (List.rev rowList,[])
+        | a :: tail when a = "TABLE" -> (List.rev rowList,inpStrings)
+        | a :: tail -> allInOneTableTwo tail (a :: rowList)
+
+    let rec wrapperTwo inpStrings tableList =
+        let (table, strings) = allInOneTableTwo inpStrings []
+        match strings with
+        | [] -> table :: tableList 
+        | "TABLE" :: tail -> wrapperTwo tail (table::tableList)
+
+    let sortIntoMultipleTwo inpStrings = 
+        let tableLists = []
+        match inpStrings with 
+        | a :: name :: tail when a = "TABLE" -> List.rev (wrapperTwo tail []) *)
+
+
+    let rec buildDatabaseRec tableList tableNames tableTypes thisNode =
+        match tableList, tableNames, tableTypes with
+        | ([],[],[]) -> ()
+        | thisTable :: tableTail, thisName :: nameTail, theseTypes :: typeTail ->
+            let nextNode = ref INilTable
+            thisNode := TableNode (thisTable, thisName, theseTypes, nextNode )
+            buildDatabaseRec tableTail nameTail typeTail nextNode
+
+    let buildDatabase tableList tableNames tableTypes =
+        let firstNode = ref INilTable
+        buildDatabaseRec tableList tableNames tableTypes firstNode
+        firstNode
+
+    let buildDatabaseWrapper inpStrings =
+        let (names, typesRaw, data) = inpStrings |> sortIntoMultipleRev |> splitIntoThreeLists
+        let types = List.map getTypes (List.map cleanLine typesRaw)
+        let tables = List.map readTable data
+        buildDatabase tables names types
+
+    let readInFull pathName = 
+        pathName |> readFile |> Seq.toList |> buildDatabaseWrapper
+ 
+
+    //let extractBoxValues thisRow =
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Tests
+
+    sortIntoMultipleRev ["TABLE"; "User Table"; "User String"; "X Y"; "Z W"; "TABLE"; "Table Two"; "ID Int"; "X Y"; "W Z"]
+    |> splitIntoThreeLists
+    getTypes (cleanLine "User  String   ID Int")
+    sortIntoMultipleRev ["TABLE";"User Table";"Names String ID Int";"String Some Orlando Int Some 45";"String Some Rebecca Int Some 42"; "TABLE"; "Table Two"; "Subject String"; "String Some Math"; "String Some Music"]
+
+    buildDatabaseWrapper ["TABLE";"User Table";"Names String ID Int";"String Some Orlando Int Some 45";"String Some Rebecca Int Some 42"; "TABLE"; "Table Two"; "Subject String"; "String Some Math"; "String Some Music"]
+
+
+    let exCol = ["Names"; "String"; "ID"; "Int"]
+    let exData = [String (Some "Hedda"); Int (Some 1)]
+    let exDataMult = [[String (Some "Hedda"); Int (Some 1)];[String (Some "Nora"); Int (Some 6)]]
+    let badData = [[String (Some "Hedvig"); Float (Some 3.2)]]
+
+    matchInputLists exCol exData
+    buildData exCol exDataMult
+    buildData exCol badData
+
+    strProcess ["String"; "Some"; "Orlando"; "Int"; "Some"; "45"]
+    strProcess ["Byte"; "Some"; "0xF3"; "Byte"; "None"]
+    strProcess ["Bool"; "Some"; "True"; "Bool"; "None"]
+    strProcess ["Float"; "Some"; "3.1415926"; "Float"; "None"]
+    strProcess ["String"; "some"; "hei"]
+    strProcess ["Strange"; "Some"; "Dragon"]
+
+    let myPath  = @"C:\Users\Sigrid\Documents\Visual Studio 2015\HLP\dataFile.txt"
+    let outPath = @"C:\Users\Sigrid\Documents\Visual Studio 2015\HLP\outFile.txt"
+    let someStruct = @"C:\Users\Sigrid\Documents\Visual Studio 2015\HLP\someStructureDataFile.txt"
+    let structPath = @"C:\Users\Sigrid\Documents\Visual Studio 2015\HLP\structuredDataFile.txt"
+    let fullPath = @"C:\Users\Sigrid\Documents\Visual Studio 2015\HLP\structuredNamedDataFile.txt" 
+
+    readInFull fullPath // Reads in data as saved with final specification (17/3/17)
+
+    let fileLines = readFile myPath
+    let k = readIn fileLines
+    let j = ReadInData myPath
+
+    let exCol2 = ["Names"; "String"; "ID"; "Int"; "Credit"; "Float"; "Hex"; "Byte"; "Member"; "Bool"]
+    let testSeparate = ["Names String ID Int";"String Some Orlando Int Some 45";"String Some Rebecca Int Some 42"]
+    readTable testSeparate
+    buildData exCol2 j
+
+    let testMultiple = ["TABLE";"Names String ID Int";"String Some Orlando Int Some 45";"String Some Rebecca Int Some 42"; "TABLE"; "Subject String"; "String Some Math"; "String Some Music"]
+
+    wrapper ["TABLE"; "User String"; "X Y"; "Z W"; "TABLE"; "ID Int"; "X Y"; "W Z"] []
+
+    // Get table names in a list, get table tyoes in a list, get a list with strings separated for tables (sortIntoMultiple output)
+
+        
+
+
     sortIntoMultiple ["TABLE"; "User String"; "X Y"; "Z W"; "TABLE"; "ID Int"; "X Y"; "W Z"]
+        
     sortIntoMultiple testMultiple
     readFile structPath |> Seq.toList |> sortIntoMultiple |> List.map readTable
         
@@ -200,3 +331,11 @@
     printfn "%A" fileLines
     printfn "%A" k
     printfn "%A" j
+
+(*
+To save database:
+    Extract list of values from a table
+    Extract column names and types from a table
+    Extract table name
+    Write "TABLE" 
+*)
