bb2ebc83d3ec7b252defc897d4da857e67220207
Sigrid Passano Hellan <sighellan@gmail.com>
2017-03-19 17:10:59 +0000

    Now takes database structure straight from the module.

--- src/ExecutionEngineCreate.fs
+++ src/ExecutionEngineCreate.fs
@@ -1,44 +1,9 @@
 ﻿namespace ExecutionEngine
-module ExecutionEngineCreate =
+module Create =
 
     open System.IO
     open System
-
-    type boxData = | String of Option<string>  // To be extended when the wanted data types have been decided
-                   | Int of Option<int> 
-                   | Float of Option<float>                
-                   | Byte of Option<byte>     
-                   | Bool of Option<bool>
-
-    // A node has the parameter name (column name), the parameter value and links to the previous and next nodes
-    type boxList = BoxNode of ParName: string * Value: boxData * Prev : ref<boxList> * Tl : ref<boxList> | INilBox 
-    type row = ref<boxList>                                                                                       
-    // The nodes of the table-level list (rowList) have refs to the row-level lists (boxList).
-    type rowList = RowNode of BoxList : ref<boxList> * Tl : ref<rowList> | INilRow                            
-    type table = ref<rowList>  // Holds all the data in the table
-    // Holds all the tables in the database.
-    // A node has a table, a table name, a list with column names and types, and a tail pointing to the next table in the database.
-    type tableList = TableNode of topList : ref<rowList> * TableName : string * Columns : (string * boxData) list * Tl : ref<tableList> | INilTable 
-    type database = ref<tableList>
-
-    // Get the tail from a table (entry after last row)
-    let rec rowListLast thisList : table =
-        match !thisList with
-        | INilRow -> thisList
-        | RowNode (_, tail) -> rowListLast tail
-
-    // Get the first row from a table
-    let rowListFirstRow (thisList : table) =
-        match !thisList with
-        | INilRow -> None
-        | RowNode (row, _) -> Some row
-
-    // Get a specific table from a database
-    let rec chooseTable thisDatabase tableName =
-        match !thisDatabase with
-        | INilTable -> None
-        | TableNode (thisTable, thisName, _, _) when thisName = tableName -> Some thisTable
-        | TableNode (_, _, _, otherTables) -> chooseTable otherTables tableName
+    open databaseStructure.databaseStructure
 
     type ReturnCode<'a> = // From Matt
         | Result of 'a
