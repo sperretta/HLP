023e1f5e23ea4cb8b84167c0f6c0d938a5ae46d6
Sigrid Passano Hellan <sighellan@gmail.com>
2017-03-20 18:58:58 +0000

    Files for tests for select.

---
+++ test/ExecutionEngineTestsSelect.fs
@@ -0,0 +1,16 @@
+﻿namespace ExecutionEngineTests
+module Select =
+
+    open System.IO
+    open System
+    open ReturnControl.Main
+    open databaseStructure.databaseStructure
+    open ExecutionEngine.Create
+    open ExecutionEngine.Insert
+    open ExecutionEngine.Delete
+    open EcecutionEngine.Select
+    open Expecto
+
+
+    [<Tests>]
+    let testExEngineSelect =
