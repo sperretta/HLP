d3ca9b88d520b0dd6045a420d3c338dd8382005e
Sigrid Passano Hellan <sighellan@gmail.com>
2017-03-20 10:11:39 +0000

    Tests for ExecutionEngine.Create.

---
+++ test/ExecutionEngineTestsCreate.fs
@@ -0,0 +1,18 @@
+﻿namespace ExecutionEngineTests
+module Create =
+    
+    open System.IO
+    open System
+    open databaseStructure.databaseStructure
+    open ExecutionEngine.Create
+    open Expecto
+
+    [<Tests>]
+    let testExEngineCreate =
+        testList "Tests for the ExecutionEngine.Create module" [
+            testCase "test 1" <| fun _ ->
+                let myDB = ref INilTable
+                let comp = ref (TableNode ( ref INilRow, "Small Table", [("Name", String None);("ID", Int None)], ref INilTable  ))
+                create "Small Table" [("Name", String None);("ID", Int None)] myDB |> ignore
+                Expect.equal (myDB) (comp) "Create a two-column table"
+        ]
