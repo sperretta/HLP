b08a0ccd20477a095edac780bea290c903375106
Simon Perretta <simon.perretta@outlook.com>
2017-03-21 02:37:57 +0000

    Started logger.

---
+++ src/Logger.fs
@@ -0,0 +1,10 @@
+﻿namespace Logger
+open System
+open System.IO
+
+module Logger = 
+    let logFile = "SharpData.log"
+    let Start = File.AppendAllText(logFile, "SharpData launched at " + (System.DateTime.Now).ToString("HH:mm dd.MM.yy") + Environment.NewLine)
+    let Stop = File.AppendAllText(logFile, "SharpData terminated at " + (System.DateTime.Now).ToString("HH:mm dd.MM.yy") + Environment.NewLine)
+    let Log n = ""
+
--- src/Program.fs
+++ src/Program.fs
@@ -1,7 +1,12 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
 
+open Logger
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    //printfn "%A" argv
+    Logger.Start
+    ///
+    Logger.Stop
     0 // return an integer exit code
